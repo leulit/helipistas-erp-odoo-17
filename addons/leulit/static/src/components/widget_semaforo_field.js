ddee3ca2d5d521d2618704011f36df6800b7b955
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2025-10-20 16:11:37 +0200

    problemas con vistas

--- addons/leulit/static/src/components/widget_semaforo_field.js
+++ addons/leulit/static/src/components/widget_semaforo_field.js
@@ -4,16 +4,15 @@ import { CharField } from '@web/views/fields/char/char_field';
 import { registry } from '@web/core/registry';
 import { BooleanField } from '@web/views/fields/boolean/boolean_field'
 
-export class SemaforoBool extends BooleanField {}
-// Vinculamos el componente a su plantilla QWeb
-SemaforoChar.template = "leulit.SemaforoBooleanField";
+export class SemaforoBool extends BooleanField {
+    static template = "leulit.SemaforoBooleanField";
+}
 
 registry.category('fields').add('semaforo_bool', SemaforoBool);
 
 
-export class SemaforoChar extends CharField {}
-
-// Vinculamos el componente a su plantilla QWeb
-SemaforoChar.template = "leulit.SemaforoCharField";
+export class SemaforoChar extends CharField {
+    static template = "leulit.SemaforoCharField";
+}
 
 registry.category("fields").add("semaforo_char", SemaforoChar);
