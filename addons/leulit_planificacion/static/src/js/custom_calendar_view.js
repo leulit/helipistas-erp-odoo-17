c7692a18920ff8d248e721cf82c3f52464ac66de
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2025-10-20 12:18:32 +0200

    planificacion js

--- addons/leulit_planificacion/static/src/js/custom_calendar_view.js
+++ addons/leulit_planificacion/static/src/js/custom_calendar_view.js
@@ -5,29 +5,77 @@ import { AttendeeCalendarController } from "@calendar/views/attendee_calendar/at
 import { onWillStart } from "@odoo/owl";
 
 patch(AttendeeCalendarController.prototype, {
-    setup() {
-        super.setup(...arguments);
-
-        // onWillStart se ejecuta ANTES de la primera carga.
-        onWillStart(async () => {
-            // Esperamos a que el modelo se cargue por primera vez con sus datos por defecto.
-            await this.model.load();
-            
-            // Justo después de la carga inicial, y ANTES del primer renderizado,
-            // modificamos los filtros en memoria.
-            const filterSections = this.model.data.filterSections;
-            if (filterSections && filterSections.partner_ids) {
-                const partnerFilters = filterSections.partner_ids.filters;
-                const allFilter = partnerFilters.find(f => f.type === 'all');
-                const userPartnerFilter = partnerFilters.find(f => f.value === this.model.userPartnerId);
-
-                if (allFilter) {
-                    allFilter.active = true;
-                }
-                if (userPartnerFilter) {
-                    userPartnerFilter.active = false;
+    
+    async updateAttendeeData(data) {
+        const attendeeFilters = data.filterSections.partner_ids;
+        let isEveryoneFilterActive = true;
+        let attendeeIds = [];
+        const eventIds = Object.keys(data.records).map(id => Number.parseInt(id));
+        if (attendeeFilters) {
+            const allFilter = attendeeFilters.filters.find(filter => filter.type === "all")
+            isEveryoneFilterActive = allFilter && allFilter.active || false;
+            attendeeIds = attendeeFilters.filters.filter(filter => filter.type !== "all" && filter.value).map(filter => filter.value);
+        }
+        data.attendees = await this.orm.call(
+            "res.partner",
+            "get_attendee_detail",
+            [attendeeIds, eventIds],
+        );
+        const currentPartnerId = this.user.partnerId;
+        if (!isEveryoneFilterActive) {
+            const activeAttendeeIds = new Set(attendeeFilters.filters
+                .filter(filter => filter.type !== "all" && filter.value && filter.active)
+                .map(filter => filter.value)
+            );
+            // Duplicate records per attendee
+            const newRecords = {};
+            let duplicatedRecordIdx = -1;
+            for (const event of Object.values(data.records)) {
+                const eventData = event.rawRecord;
+                const attendees = eventData.partner_ids && eventData.partner_ids.length ? eventData.partner_ids : [eventData.partner_id[0]];
+                let duplicatedRecords = 0;
+                for (const attendee of attendees) {
+                    if (!activeAttendeeIds.has(attendee)) {
+                        continue;
+                    }
+                    // Records will share the same rawRecord.
+                    const record = { ...event };
+                    const attendeeInfo = data.attendees.find(a => (
+                        a.id === attendee &&
+                        a.event_id === event.id
+                    ));
+                    record.attendeeId = attendee;
+                    // Colors are linked to the partner_id but in this case we want it linked
+                    // to attendeeId
+                    record.colorIndex = attendee;
+                    if (attendeeInfo) {
+                        record.attendeeStatus = attendeeInfo.status;
+                        record.isAlone = attendeeInfo.is_alone;
+                        record.isCurrentPartner = attendeeInfo.id === currentPartnerId;
+                        record.calendarAttendeeId = attendeeInfo.attendee_id;
+                    }
+                    const recordId = duplicatedRecords ? duplicatedRecordIdx-- : record.id;
+                    // Index in the records
+                    record._recordId = recordId;
+                    newRecords[recordId] = record;
+                    duplicatedRecords++;
                 }
             }
-        });
+            data.records = newRecords;
+        } else {
+            for (const event of Object.values(data.records)) {
+                const eventData = event.rawRecord;
+                event.attendeeId = eventData.partner_id && eventData.partner_id[0]
+                const attendeeInfo = data.attendees.find(a => (
+                    a.id === currentPartnerId &&
+                    a.event_id === event.id
+                ));
+                if (attendeeInfo) {
+                    event.isAlone = attendeeInfo.is_alone;
+                    event.calendarAttendeeId = attendeeInfo.attendee_id;
+                }
+            }
+        }
     }
+
 });
