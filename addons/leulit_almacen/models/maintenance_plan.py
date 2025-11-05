5da7055e1757bf26bb1100fb6b7437cc88d2c174
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2025-11-05 16:39:19 +0100

    Refactor get_all_childs method in MaintenanceEquipment model: remove logging for cleaner code and improve performance

--- addons/leulit_almacen/models/maintenance_plan.py
+++ addons/leulit_almacen/models/maintenance_plan.py
@@ -15,14 +15,10 @@ class MaintenancePlan(models.Model):
     def _get_componentes_from_equipment(self):
         for item in self:
             lots = []
-            if item.equipment_id:
-                _logger.error("Equipment ID: %s", item.equipment_id.id)
-                for child in item.equipment_id.get_all_childs():
-                    _logger.error("Child Equipment ID: %s", child.id)
-                    if child.production_lot:
-                        _logger.error("Adding Production Lot ID: %s", child.production_lot.id)
-                        lots.append(child.production_lot.id)
-            item.componentes = [(6, 0, lots)]
+            for child in item.equipment_id.get_all_childs():
+                if child.production_lot:
+                    lots.append(child.production_lot.id)
+            item.componentes = lots
 
     
-    componentes = fields.Many2many(compute=_get_componentes_from_equipment, comodel_name="stock.lot", string="Componentes")
+    componentes = fields.One2many(compute=_get_componentes_from_equipment, comodel_name="stock.lot", inverse_name="plan_id", string="Componentes")
