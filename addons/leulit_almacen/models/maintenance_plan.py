000f8e1fbb92398c2227804123f3bc0ca8a372ad
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2025-11-05 16:26:13 +0100

    Refactor _get_componentes_from_equipment method in MaintenancePlan model: enhance logging for better traceability of equipment and production lot IDs

--- addons/leulit_almacen/models/maintenance_plan.py
+++ addons/leulit_almacen/models/maintenance_plan.py
@@ -16,8 +16,11 @@ class MaintenancePlan(models.Model):
         for item in self:
             lots = []
             if item.equipment_id:
+                _logger.error("Equipment ID: %s", item.equipment_id.id)
                 for child in item.equipment_id.get_all_childs():
+                    _logger.error("Child Equipment ID: %s", child.id)
                     if child.production_lot:
+                        _logger.error("Adding Production Lot ID: %s", child.production_lot.id)
                         lots.append(child.production_lot.id)
             item.componentes = [(6, 0, lots)]
 
