147c190cfd4769cc47883c51a4270df8f15a7da5
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2025-11-11 11:08:54 +0100

    Implement asynchronous recalculation of complete_name for partners, enhancing performance and user experience by processing in a separate thread.

--- addons/leulit/models/res_partner.py
+++ addons/leulit/models/res_partner.py
@@ -5,6 +5,8 @@ from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationE
 import logging
 from datetime import datetime
 from odoo.addons.leulit import utilitylib
+import threading
+import warnings
 
 _logger = logging.getLogger(__name__)
 
@@ -55,19 +57,70 @@ class res_partner(models.Model):
                 codigo_max_cobrar = codigo_max_cobrar+1
             except Exception as e:
                 raise UserError ('No se ha podido asignar las cuentas, comunicalo a IT (code intentado: %s / %s).' % (str(codigo_max_cobrar+1),str(codigo_max_pagar+1)))
+
+    def _recalcular_complete_name_thread(self, dbname, uid):
+        """Función interna que se ejecuta en el thread"""
+        with api.Environment.manage():
+            new_cr = registry(dbname).cursor()
+            try:
+                env = api.Environment(new_cr, uid, {})
+                warnings.filterwarnings('ignore')
+                
+                partners = env['res.partner'].search([])
+                total = len(partners)
+                _logger.info('Iniciando recálculo de complete_name para %s partners', total)
+                
+                for idx, partner in enumerate(partners, 1):
+                    try:
+                        partner._compute_complete_name()
+                        if idx % 100 == 0:
+                            _logger.info('Procesados %s/%s partners', idx, total)
+                            new_cr.commit()
+                    except Exception as e:
+                        _logger.error('Error al calcular complete_name para partner %s: %s', partner.id, str(e))
+                
+                new_cr.commit()
+                _logger.info('Recálculo de complete_name completado para %s partners', total)
+            except Exception as e:
+                _logger.error('Error en thread de recálculo: %s', str(e))
+                new_cr.rollback()
+            finally:
+                new_cr.close()
+
+    def recalcular_complete_name_async(self):
+        """Método público para lanzar el recálculo en un thread"""
+        if not self.env.user.has_group('base.group_system'):
+            raise UserError('Solo los administradores pueden ejecutar esta acción.')
+        
+        dbname = self.env.cr.dbname
+        uid = self.env.uid
+        
+        thread = threading.Thread(
+            target=self._recalcular_complete_name_thread,
+            args=(dbname, uid)
+        )
+        thread.daemon = True
+        thread.start()
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'Recálculo iniciado',
+                'message': 'El recálculo de complete_name se está ejecutando en segundo plano. Revisa los logs para ver el progreso.',
+                'type': 'success',
+                'sticky': False,
+            }
+        }
                 
 
 
     bool_cuentas_custom = fields.Boolean(string="",default=False)
-
     firma = fields.Binary('Firma')
     sello = fields.Binary('Sello')
-
     peso_piloto = fields.Float('Peso Piloto')
     country_id = fields.Many2one('res.country', string='Country', ondelete='restrict',default=68)
     lang = fields.Selection(_lang_get, string='Language',help="All the emails and documents sent to this contact will be translated in this language.", default="es_ES")
-
-
     alumno_erp = fields.Integer(string='alumno_erp')
     piloto_erp = fields.Integer(string='piloto_erp')
     profesor_erp = fields.Integer(string='profesor_erp')
