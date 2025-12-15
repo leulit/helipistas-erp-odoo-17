abafefeea52f0db29c5c9919be8a3af88cdb1826
ealvarezreb <ealvarez@leulit.com>
2025-12-15 13:45:17 +0100

    feat: Add post-init hook to remove duplicate root menu after migration

--- addons/leulit_partis/__init__.py
+++ addons/leulit_partis/__init__.py
@@ -1,1 +1,8 @@
 from . import models
+
+
+def post_init_hook(env):
+    """Remove duplicate root menu after migration."""
+    menu = env.ref('leulit_partis.menu_leulit_partis_root', raise_if_not_found=False)
+    if menu:
+        menu.unlink()
--- addons/leulit_partis/__manifest__.py
+++ addons/leulit_partis/__manifest__.py
@@ -30,4 +30,5 @@
     ],
     "installable": True,
     "application": False,
+    "post_init_hook": "post_init_hook",
 }
