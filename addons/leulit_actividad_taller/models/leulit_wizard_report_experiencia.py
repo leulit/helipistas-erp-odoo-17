d91e638d78ca6b6d1547d29e239329132077b3d9
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-02-19 12:04:02 +0100

    fix(leulit_experiencia): add logging for report data and correct items parameter in report action

--- addons/leulit_actividad_taller/models/leulit_wizard_report_experiencia.py
+++ addons/leulit_actividad_taller/models/leulit_wizard_report_experiencia.py
@@ -68,4 +68,6 @@ class leulit_wizard_report_experiencia(models.TransientModel):
             'to_date': self.to_date.strftime('%d/%m/%Y') if self.to_date else '',
             # 'total_pages': len(pages)
         }
-        return self.env.ref('leulit_actividad_taller.leulit_20240521_1012_report').report_action(items.ids,data=datos)
+        _logger.error('Datos para el reporte: %s', datos)
+        _logger.error('Items para el reporte: %s', items)
+        return self.env.ref('leulit_actividad_taller.leulit_20240521_1012_report').report_action(items,data=datos)
