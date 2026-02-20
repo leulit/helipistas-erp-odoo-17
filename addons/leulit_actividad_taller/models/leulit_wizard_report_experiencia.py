9b743e7183ad79c52b372cb91f3fbff62b55723d
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-02-20 10:48:28 +0100

    fix(leulit_experiencia): correct typo in eurocopter field name and adjust form layout in experiencia report

--- addons/leulit_actividad_taller/models/leulit_wizard_report_experiencia.py
+++ addons/leulit_actividad_taller/models/leulit_wizard_report_experiencia.py
@@ -19,7 +19,7 @@ class leulit_wizard_report_experiencia(models.TransientModel):
     from_date = fields.Date('Desde')
     to_date = fields.Date('Hasta')
     ata_ids = fields.Many2many(comodel_name='leulit.ata', relation='rel_report_experiencia_ata', column1='item_report_experiencia_id', column2='ata_id', string='ATAs')
-    eurocpoter = fields.Boolean(string='Eurocopter')
+    eurocopter = fields.Boolean(string='Eurocopter')
     robinson = fields.Boolean(string='Robinson')
     guimbal = fields.Boolean(string='Guimbal')
     certificaciones_ids = fields.Many2many(comodel_name='leulit.certificacion', relation='leulit_report_experiencia_certificacion_rel', column1='item_report_experiencia_id', column2='certificacion_id', string='Certificaciones')
@@ -27,11 +27,22 @@ class leulit_wizard_report_experiencia(models.TransientModel):
 
     def print_report_experiencia(self):
         company_icarus = self.env['res.company'].search([('name','like','Icarus')])
-        items = self.env['leulit.item_experiencia_mecanico'].search([
-            ('mecanico_id', '=', self.mecanico_id.id),
-            ('date', '>=', self.from_date),
-            ('date', '<=', self.to_date)
-        ])
+        domain = [('mecanico_id', '=', self.mecanico_id.id),('date', '>=', self.from_date),('date', '<=', self.to_date)]
+        ac_type = []
+        if self.eurocopter:
+            ac_type.append('Eurocopter')
+        if self.robinson:
+            ac_type.append('Robinson')
+        if self.guimbal:
+            ac_type.append('Guimbal')
+        if ac_type:
+            domain.append(('ac_type', 'in', ac_type))
+        if self.ata_ids:
+            domain.append(('ata_ids', 'in', self.ata_ids.ids))
+        if self.certificaciones_ids:
+            domain.append(('privilege', 'in', self.certificaciones_ids.ids))
+
+        items = self.env['leulit.item_experiencia_mecanico'].search(domain)
         datos = {
             'logo_ica': company_icarus.logo_reports.decode(),
             'mecanico': self.mecanico_id.name if self.mecanico_id else '',
