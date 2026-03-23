30d98dabe4530a1faa238897c93b9e0689b8cab5
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-03-23 14:47:43 +0100

    fix(mgmtsystem_nonconformity): implement custom write method to handle create_date updates

--- addons/leulit_seguridad/models/mgmtsystem_nonconformity.py
+++ addons/leulit_seguridad/models/mgmtsystem_nonconformity.py
@@ -19,3 +19,14 @@ class MgmtsystemNonconformity(models.Model):
     )
     partner_id = fields.Many2one("res.partner", "Partner", required=False)
     create_date = fields.Datetime("Creado en", readonly=False)
+
+    def write(self, vals):
+        create_date = vals.pop("create_date", None)
+        res = super().write(vals)
+        if create_date:
+            self._cr.execute(
+                "UPDATE mgmtsystem_nonconformity SET create_date = %s WHERE id IN %s",
+                (create_date, tuple(self.ids)),
+            )
+            self.invalidate_recordset(["create_date"])
+        return res
