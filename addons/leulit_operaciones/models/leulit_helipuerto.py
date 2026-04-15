ad633c6994595209218d54559a849e6758d1cbb1
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-04-15 11:12:16 +0200

    feat(leulit_helipuerto): add fecha_edicion_revision field and update form layout

--- addons/leulit_operaciones/models/leulit_helipuerto.py
+++ addons/leulit_operaciones/models/leulit_helipuerto.py
@@ -121,6 +121,7 @@ class leulit_helipuerto(models.Model):
     img_3 = fields.Image('Imagen 3')
     img_4 = fields.Image('Imagen 4')
     edicion_revision = fields.Char('Edición y revisión', required=True)
+    fecha_edicion_revision = fields.Date('Fecha edición y revisión', required=True)
     puntos_ids = fields.One2many(comodel_name="leulit.ruta_punto", inverse_name="helipuerto_id", string="Puntos")
     punto_generado = fields.Boolean(compute=_get_punto_generado, string="¿Punto generado?")
     seguimiento_comercial = fields.Boolean('Seguimiento comercial')
