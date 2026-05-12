23f8d097f68dfbc59497ded11f34bb9105c762d9
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-05-12 11:10:24 +0200

    Add 'is_developer_user' field to project.project model and update view for visibility control

--- addons/leulit/models/project_project.py
+++ addons/leulit/models/project_project.py
@@ -6,3 +6,10 @@ class ProjectProject(models.Model):
     _inherit = 'project.project'
 
     privacy_visibility = fields.Selection(default='followers')
+
+    is_developer_user = fields.Boolean(compute='_compute_is_developer_user')
+
+    def _compute_is_developer_user(self):
+        is_dev = self.env.user.has_group('leulit.RolIT_developer')
+        for record in self:
+            record.is_developer_user = is_dev
