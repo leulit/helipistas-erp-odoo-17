6ce2d9d7e4679894a30894d57681b8cbc06a03d8
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-05-13 16:04:50 +0200

    Enhance hazard management: Add action_ids field and update view titles for clarity

--- addons/leulit_seguridad/models/mgmtsystem_hazard.py
+++ addons/leulit_seguridad/models/mgmtsystem_hazard.py
@@ -4,5 +4,13 @@ from odoo import fields, models
 
 class MgmtsystemHazard(models.Model):
     _inherit = "mgmtsystem.hazard"
+    _description = "Análisis de Riesgos"
 
     risk_type_id = fields.Many2one(string="Peligro")
+    action_ids = fields.Many2many(
+        "mgmtsystem.action",
+        "leulit_hazard_action_rel",
+        "hazard_id",
+        "action_id",
+        string="Acciones",
+    )
