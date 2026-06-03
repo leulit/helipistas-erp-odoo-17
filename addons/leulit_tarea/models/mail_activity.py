4a36eeec9da6d5801712ad638de1a4cb569d20b6
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-06-03 11:40:29 +0200

    Add deadline validation for mail activities linked to tasks and actions

--- addons/leulit_tarea/models/mail_activity.py
+++ addons/leulit_tarea/models/mail_activity.py
@@ -1,10 +1,37 @@
 # -*- encoding: utf-8 -*-
-from odoo import models, _
-from odoo.exceptions import UserError
+from odoo import models, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_MODELOS_CON_FECHA_LIMITE = ('project.task', 'mgmtsystem.action')
 
 
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
+
+    @api.constrains('date_deadline', 'res_model', 'res_id')
+    def _check_deadline_vs_parent(self):
+        for activity in self:
+            if (
+                not activity.date_deadline
+                or activity.res_model not in _MODELOS_CON_FECHA_LIMITE
+                or not activity.res_id
+            ):
+                continue
+            parent_model = self.env.get(activity.res_model)
+            if parent_model is None:
+                continue
+            parent = parent_model.browse(activity.res_id)
+            if not parent.exists() or not parent.date_deadline:
+                continue
+            if activity.date_deadline > parent.date_deadline:
+                tipo = _("tarea") if activity.res_model == 'project.task' else _("acción")
+                raise ValidationError(_(
+                    "La fecha de vencimiento de la actividad (%s) no puede ser posterior "
+                    "a la fecha límite de la %s (%s).",
+                    activity.date_deadline,
+                    tipo,
+                    parent.date_deadline,
+                ))
 
     def action_open_source_record(self):
         self.ensure_one()
