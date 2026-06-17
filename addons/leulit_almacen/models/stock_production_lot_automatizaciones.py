106efa190be669c96cb220fdb318dde842264de9
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-06-17 10:05:32 +0200

    Update email notification to send to specific recipients for expiration alerts

--- addons/leulit_almacen/models/stock_production_lot_automatizaciones.py
+++ addons/leulit_almacen/models/stock_production_lot_automatizaciones.py
@@ -35,7 +35,4 @@ class StockLot(models.Model):
                 'email_values': email_values,
                 'values_flag': True if len(email_values) > 0 else False,
             }
-            email_to = ['almacen@icarus-manteniment.com', 'otecnica@helipistas.com', 'ops@helipistas.com']
-            for email in email_to:
-                email_context['email_to'] = email
-                template.with_context(email_context).send_mail(self.env.user.id, force_send=True)
+            template.with_context(email_context).send_mail(self.env.user.id, force_send=True)
