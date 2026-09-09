51c779230174463f7c392cffe7fac9bda80a5cf9
Pau Barbarroja Claramunt <paubarbarroja@gmail.com>
2026-09-09 11:37:37 +0200

    feat(maintenance_manual): add 'nombre_archivo' field and update XML view to display it

--- addons/leulit_taller/models/leulit_maintenance_manual.py
+++ addons/leulit_taller/models/leulit_maintenance_manual.py
@@ -71,6 +71,7 @@ class leulitMaintenanceManual(models.Model):
             
 
     attachment_id = fields.Many2one(comodel_name="ir.attachment", string="Documento", required=True, ondelete='cascade')
+    nombre_archivo = fields.Char(string="Nombre archivo")
     categoria_id = fields.Many2one(comodel_name="leulit.categoria_manual", string="Categoria")
     descripcion = fields.Char(string="Descripción")
     pn = fields.Char(string="P/N")
